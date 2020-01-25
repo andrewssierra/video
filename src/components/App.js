ea976b6c4438162ae44f66aa27bd9e2a9845c9fc
andrewssierra <andrewssierra@yahoo.com>
2020-01-25 13:46:19 -0800

    Adding images and titles

--- src/components/App.js
+++ src/components/App.js
@@ -1,20 +1,28 @@
-import React from 'react';
+import React, { Component } from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
+import VideoList from './VideoList';
 
-class App extends React.Component {
-    onTermSubmit = term => {
-        youtube.get('/search', {
+class App extends Component {
+    state = { videos: [] };
+
+    onTermSubmit = async term => {
+        let response = await youtube.get('/search', {
             params: {
                 q: term
             }
         });
+
+        this.setState({ videos: response.data.items });
     };
 
     render() {
+        const videosLength = this.state.videos.length;
         return (
             <div className="ui container">
                 <SearchBar onTermSubmit={this.onTermSubmit} />
+                <VideoList videos={this.state.videos} />
+                {videosLength > 0 ? `${videosLength} found` : null}
             </div>
         );
     }
